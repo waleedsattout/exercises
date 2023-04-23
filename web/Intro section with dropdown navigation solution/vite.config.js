14f664aaabffb46dc7f7b65fd67fd04647d06a60
waleedsattout <sattout.2002@gmail.com>
2023-04-23 12:13:18 +0300

    fix base url

--- web/Intro section with dropdown navigation solution/vite.config.js
+++ web/Intro section with dropdown navigation solution/vite.config.js
@@ -4,4 +4,5 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(),],
+  base: 'exercises/web/Intro%20section%20with%20dropdown%20navigation%20solution/'
 });
